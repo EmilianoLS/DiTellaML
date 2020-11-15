7d0d2d90c7cf821bffb1ad730f73fb5b2b917392
Emiliano <emiliano.losasso@telefonica.com>
2020-11-15 12:42:55 -0300

    Update test_script.py

--- Tecnicas Algoritmicas/test_script.py
+++ Tecnicas Algoritmicas/test_script.py
@@ -56,7 +56,7 @@ def find_students_topics_to_change(solution, prefs):
 
 
 # Cargo un ejemplo de preferencias.
-prefs = Preferencias("Ejemplo10")
+prefs = Preferencias("Ejemplo50")
 
 # Determino una primera solucion con la heurísitica 'greedy'
 solucion_actual = asignar_greedy(prefs)
@@ -69,69 +69,62 @@ students_to_change, topics_to_change = find_students_topics_to_change(solucion_a
 
 pudimos_mejorar = True
 
-while pudimos_mejorar:
-
-    costo_mejor_vecino = float('inf')
-    mejor_vecino = None
+if costo_solucion_actual != 0:
+    while pudimos_mejorar:
     
-    for student in range(0, len(students_to_change)-1):
+        costo_mejor_vecino = float('inf')
+        mejor_vecino = None
         
-        for topic in range(student+1,len(topics_to_change)):
+        for student in range(0, len(students_to_change)-1):
             
-            vecino = solucion_actual.copy()
-            
-            # Encuentro el topico asignado al estudiante 'e'
-            # assigned_topic es EL topico, no es un indice
-            assigned_topic = vecino.topico_asignado_a_estudiante(students_to_change[student])
-            
-            # Desasigno el topico correspondiente al estudiante 'e'
-            vecino.desasignar(students_to_change[student], assigned_topic) 
-            
-            # Encuentro el estudiante asignado al 'topic'
-            # assigned_student es EL student, no es un indice 
-            assigned_student = vecino.estudiante_asignado_a_topico(topics_to_change[topic])
-            
-            # Desasigno el topico 'topic' del estudiante correspondiente
-            vecino.desasignar(assigned_student, topics_to_change[topic])
-            
-            # Intercambio los topicos entre el 'student' y el 'assigned_student'
-            # Asigno al student
-            vecino.asignar(students_to_change[student], topics_to_change[topic])
-            # Asigno al assigned_student
-            vecino.asignar(assigned_student, assigned_topic)
-            
-            # Calculo el costo de la nueva asignacion
-            costo = vecino.calcular_costo()
-            
-            if costo < costo_mejor_vecino:
-                costo_mejor_vecino = costo
-                mejor_vecino = vecino
-            
-    # Si el mejor vecino encontrado es mejor que la solución actual,
-    # lo guardamos y seguimos buscando. Si no, terminamos.
-    
-    if costo_mejor_vecino < costo_solucion_actual:
-        print('Costo solucion actual: ', costo_solucion_actual)
-        print('Costo mejor vecino: ', costo_mejor_vecino)
-        pudimos_mejorar = True
-        solucion_actual = mejor_vecino
-        costo_solucion_actual = costo_mejor_vecino
-        # Vuelvo a encontrar los estudiantes que intercambiaran topicos
-        students_to_change, topics_to_change = find_students_topics_to_change(solucion_actual, prefs)
-    else:
-        pudimos_mejorar = False
+            for topic in range(student+1,len(topics_to_change)):
+                
+                vecino = solucion_actual.copy()
+                
+                # Encuentro el topico asignado al estudiante 'e'
+                # assigned_topic es EL topico, no es un indice
+                assigned_topic = vecino.topico_asignado_a_estudiante(students_to_change[student])
+                
+                # Desasigno el topico correspondiente al estudiante 'e'
+                vecino.desasignar(students_to_change[student], assigned_topic) 
+                
+                # Encuentro el estudiante asignado al 'topic'
+                # assigned_student es EL student, no es un indice 
+                assigned_student = vecino.estudiante_asignado_a_topico(topics_to_change[topic])
+                
+                # Desasigno el topico 'topic' del estudiante correspondiente
+                vecino.desasignar(assigned_student, topics_to_change[topic])
+                
+                # Intercambio los topicos entre el 'student' y el 'assigned_student'
+                # Asigno al student
+                vecino.asignar(students_to_change[student], topics_to_change[topic])
+                # Asigno al assigned_student
+                vecino.asignar(assigned_student, assigned_topic)
+                
+                # Calculo el costo de la nueva asignacion
+                costo = vecino.calcular_costo()
+                
+                if costo < costo_mejor_vecino:
+                    costo_mejor_vecino = costo
+                    mejor_vecino = vecino
+                
+        # Si el mejor vecino encontrado es mejor que la solución actual,
+        # lo guardamos y seguimos buscando. Si no, terminamos.
         
-   
-lista1 = [1,2,3]
-lista2 = [4,5,6]
+        if costo_mejor_vecino < costo_solucion_actual:
+            print('Costo solucion actual: ', costo_solucion_actual)
+            print('Costo mejor vecino: ', costo_mejor_vecino)
+            pudimos_mejorar = True
+            solucion_actual = mejor_vecino
+            costo_solucion_actual = costo_mejor_vecino
+            # Vuelvo a encontrar los estudiantes que intercambiaran topicos
+            students_to_change, topics_to_change = find_students_topics_to_change(solucion_actual, prefs)
+        else:
+            pudimos_mejorar = False
+        
+else:
+    print('La solucion ya es optima!')
 
-for i in range(0, len(lista1)-1):
-    for j in range(i+1, len(lista2)):
-        print('Student: ',lista1[i]) 
-        print('Topic to remove: ', lista2[i])
-        print('Topic to assign: ', lista2[j])         
-        
-        
     
     
     
